a287c067a2f74362c7f0a0a6a3d9934925ad6933
anthonykravchishin <tonykravc53@gmail.com>
2025-10-28 10:51:14 -0700

    EJS integration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-// Import the ezpress module
+// Import the express module
 import express from 'express';
 
 // Create an instance of an Express application
 const app = express();
+
+// Set EJS as out view engine
+app.set('view engine', 'ejs');
 
 // Define the port number where out server will listen
 const PORT = 3000;
@@ -18,23 +21,27 @@ const orders = [];
 
 // Define a default "route" ('/')
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    // res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 });
 
 // Define a "contact-us" route
 app.get('/contact-us', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+    // res.sendFile(`${import.meta.dirname}/views/contact.html`);
+    res.render('contact');
 });
 
 // Define a "confirm" route
 app.get('/confirm', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    // res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    res.render('confirmation', { order});
 });
 
 // Define a "admin" route
 app.get('/admin', (req, res) => {
-    res.send(orders);
+    // res.send(orders);
     // res.sendFile(`${import.meta.dirname}/views/admin.html`);
+    res.render('admin');
 });
 
 // Define a "submit-route" route
@@ -47,11 +54,13 @@ app.post('/submit-order', (req, res) => {
         method: req.body.method,
         toppings: req.body.toppings,
         size: req.body.size,
+        timestamp: new Date()
     }
     orders.push(order);
     console.log(orders);
 
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    // res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    res.render('confirmation', { order});
 });
 
 // Start the server and listen on the specific port
